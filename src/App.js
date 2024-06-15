cb073aa81d2b17e6b60404c4385ff0663871a6ba
Prateek Patel <prateekpatel307@gmail.com>
2024-06-15 12:02:45 +0530

    complete manage state and CRUD functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Typography, Box } from "@mui/material";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const updateTask = (oldTask, newTask) => {
+    setTasks(tasks.map((task) => (task === oldTask ? newTask : task)));
+  };
+
+  const deleteTask = (taskToDelete) => {
+    setTasks(tasks.filter((task) => task !== taskToDelete));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box textAlign="center" my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Itinerary Planner
+        </Typography>
+        <TaskInput addTask={addTask} />
+        <TaskList
+          tasks={tasks}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+        />
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
